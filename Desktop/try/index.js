aab56a6168a6a6cec77781f967170014b47f68b0
UmuganwaXaviere <umuganwaxaviere01@gmail.com>
2023-04-28 14:05:35 +0200

    @skdsfjlk@

---
+++ Desktop/try/index.js
@@ -0,0 +1,51 @@
+// function fxName(parameter){
+//     //codes
+//     return IDBCursorWithValue;
+// }
+
+function sum(n,m){
+    return n+m;
+}
+
+console.log(`sum is ${sum(45,68)}`)
+
+
+const add = function(a,b){
+    return a+b;
+}
+console.log(`addition is ${add(45,68)}`)
+
+
+const teranya = (n,m) => n+m;
+
+console.log(`igiteranyo ni ${teranya(45,68)}`)
+
+
+function display(fname){
+    console.log(`Hello, ${fname}`
+}
+
+display('Yehudi')
+display('Carmelle')
+display('Roger')
+display('Eliola')
+
+
+disp("Chanissa")
+
+const dis = (fname)=> console.log(`Hello, ${fname}`)
+dis("Remy")
+
+// built-in function
+//   1. math function
+//   2. string function
+//   3. array  function
+//   4. date function
+//   5. Type conversion
+//   6. other
+
+// const myFunc = (Fname) => console.log('Hello, World!')
+
+setTimeout(myFunc,3000,"Yehudi")
+function doSomething(callBack){
+    console.log
